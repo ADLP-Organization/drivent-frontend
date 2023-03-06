63ac1e55e8a74a4302f6ceee50cda13aa395e6c8
Analice <anadias.alice@gmail.com>
2023-03-06 16:04:53 -0300

    feat: pag payment-credentials

---
+++ src/pages/Dashboard/PaymentCredentials/index.js
@@ -0,0 +1,219 @@
+import React from 'react';
+import api from './api';
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+//import PaymentForm from '../../../components/CreditCard/index';
+import 'react-credit-cards/es/styles-compiled.css';
+import Cards from 'react-credit-cards';
+import { ticketType } from '../../../services/ticketApi';
+import useToken from '../../../hooks/useToken';
+
+let submitData = {
+  cvc: '',
+  expiry: '',
+  name: '',
+  number: '',
+  issuer: '',
+};
+
+export class PaymentForm extends React.Component {
+  state = {
+    cvc: '',
+    expiry: '',
+    focus: '',
+    name: '',
+    number: '',
+    issuer: '',
+  };
+
+  handleCallback = ({ issuer }) => {
+    this.setState({ issuer });
+    if(typeof issuer === 'string' && issuer !== 'unknown');
+    submitData.issuer = issuer;
+  };
+
+  handleInputFocus = (e) => {
+    this.setState({ focus: e.target.name });
+  };
+  
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if(name === 'number') {
+      submitData.number = value;
+    }else if(name === 'name') {
+      submitData.name = value;
+    }else if(name === 'cvc') {
+      submitData.cvc = value;
+    }else if(name === 'expiry') {
+      submitData.expiry = value;
+    }
+    this.setState({ [name]: value });
+  };
+ 
+  render() {
+    return (
+      <PaymentFormContainer id="PaymentForm">
+        
+        <Cards
+          cvc={this.state.cvc}
+          expiry={this.state.expiry}
+          focused={this.state.focus}
+          name={this.state.name}
+          number={this.state.number}
+          callback={this.handleCallback}
+        />
+        
+        <form onSubmit= {(e) => submit(e)}>
+          <InputsContainer>
+            <div>
+              <input
+                type="tel"
+                name="number"
+                placeholder="Card Number"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+                required
+                pattern='[\d| ]{16,22}'
+                maxLength='19'
+              />
+            </div>
+            <div>
+              <input
+                type="tel"
+                name="name"
+                placeholder="Name"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+                required
+                pattern='[a-z A-Z-]+'
+              />
+            </div>
+            
+            <div>
+              <input
+                type="tel"
+                name="expiry"
+                placeholder="Valid Thru"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+                required
+                maxLength='4'
+              />
+              <input
+                type="tel"
+                name="cvc"
+                placeholder="CVC"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+                required
+                maxLength='3'
+              />             
+            </div>
+          </InputsContainer>
+        </form>
+      </PaymentFormContainer>
+    );
+  }
+}
+
+async function submit(userToken, data) {
+  const body = {
+    ticketId: 32,
+    cardData: {
+      cardIssuer: data.issuer,
+      cardLastDigits: data.number.substring((data.number.length-4), (data.number.length))
+    }
+  };
+
+  const response = await api.post('/paymentProcess', body, {
+    headers: {
+      Authorization: `Bearer ${userToken}`,
+    },
+  });
+  console.log(response);
+}
+
+export default function PaymentCredentials() {
+  const token = useToken();
+  return (
+    <>
+      <StyledTypography variant="h4">Ingresso e pagamento </StyledTypography>
+      <Summary>Resumo</Summary>
+      <Payment>
+        <Subtitle>Pagamento</Subtitle>
+        <PaymentForm />
+        <SubmitButton
+          onClick = {() => submit(token, submitData)}
+        >FINALIZAR PAGAMENTO</SubmitButton>
+      </Payment>
+      
+    </>
+  );
+}
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
+
+const Summary = styled.div`
+  height: 200px;
+  width: 500px;
+  background: red;
+`;
+
+const Payment = styled.div`
+background-color: yellow;
+  display: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PaymentFormContainer = styled.div`
+  background-color: blue;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 70px;
+`;
+
+const SubmitButton = styled.button`
+  width: 182px;
+  height: 37px;
+  background: #E0E0E0;
+  border: none;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: center;
+
+  color: #000000;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+`;
+
+const InputsContainer = styled.div`
+  background-color: pink;
+  display: column;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+`;
+
+const Subtitle = styled.div`
+  height: 23px;
+  width: 411px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 23px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #8E8E8E;
+  margin-bottom: 17px;
+`;
