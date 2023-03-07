3e37db29ae6df4944562c46c028a897398e1a1ea
Analice <anadias.alice@gmail.com>
2023-03-07 17:22:54 -0300

    refactor

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ import { EventInfoProvider } from './contexts/EventInfoContext';
 import { UserProvider } from './contexts/UserContext';
 
 import useToken from './hooks/useToken';
-import PaymentCredentials from './pages/Dashboard/PaymentCredentials';
 
 export default function App() {
   return (
