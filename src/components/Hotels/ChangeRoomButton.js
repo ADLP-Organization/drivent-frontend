98c85ed7ac646ba876eff034af5201bb4675d58f
Analice <anadias.alice@gmail.com>
2023-03-12 16:53:09 -0300

    feat: ChangeRoomButton

---
+++ src/components/Hotels/ChangeRoomButton.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import MuiButton from '@material-ui/core/Button';
+
+export default function ChangeRoomButton({ setBookingStatus }) {
+  async function returnRoomsPage() {
+    try{
+      setBookingStatus('selected');
+    } catch(err) {
+      alert('Ops, deu ruim');
+    }
+  }
+  return(
+    <Button onClick={() => returnRoomsPage()}>
+        TROCAR DE QUARTO
+    </Button>
+  );
+}
+
+const Button = styled(MuiButton)`
+    margin-top: 38px !important;
+    width: 182px;
+    height: 37px;
+    left: 0px;
+    top: 0px;
+    background: #E0E0E0;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 4px;
+`;
