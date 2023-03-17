d33cb52a8850a3795d5a943f474d24eecc34d42e
Analice <anadias.alice@gmail.com>
2023-03-16 22:09:20 -0300

    testante Inscrever-se em atividade

--- src/components/Activities/ActivitiesByDay.js
+++ src/components/Activities/ActivitiesByDay.js
@@ -1,4 +1,15 @@
+import useToken from '../../hooks/useToken';
+import { postActivity } from '../../services/activitiesApi';
+
 export default function ActivitiesByDay() {
+  const token = useToken();
+  async function testeInscreverEmAtividade() {
+    const body = {
+
+    };
+
+    await postActivity();
+  }
   return (
     'oi, aqui é pra selecionar a atividade'
   );
