d1063593181fba227d4f68410995ff546b29e20d
Analice <anadias.alice@gmail.com>
2023-03-19 15:42:44 -0300

    merge

--- src/components/Activities/EventDays.js
+++ src/components/Activities/EventDays.js
@@ -1,5 +1,8 @@
+import ActivitiesByDay from './ActivitiesByDay';
+
 export default function EventDays() {
   return (
-    'oi, aqui é pra selecionar o dia e, se o dia estiver selecionado, exibir o componente ActivitiesByDay'
+    //'oi, aqui é pra selecionar o dia e, se o dia estiver selecionado, exibir o componente ActivitiesByDay'
+    <ActivitiesByDay/> //
   );
 };
